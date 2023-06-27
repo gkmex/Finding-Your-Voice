e4b25f0b90ce329000d0f3451191a41c053f28b9
gkmex <136693704+gkmex@users.noreply.github.com>
2023-06-28 01:32:13 +0700

    Improve data validation checks

---
+++ king_sun_ocean.go
@@ -0,0 +1,386 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// 1st Line
+	fmt.Println("Find your own voice and make it heard.")
+
+	// 2nd Line
+	fmt.Println("We often feel intimidated or afraid to express ourselves.")
+
+	// 3rd Line
+	fmt.Println("It can be difficult to find our own authentic voice,")
+
+	// 4th Line
+	fmt.Println("but it’s an important part of finding our identity.")
+
+	// 5th Line
+	fmt.Println("The ability to speak up and express our own opinion")
+
+	// 6th Line 
+	fmt.Println("can be vital to our development and success.")
+
+	// 7th Line
+	fmt.Println("The first step to finding your voice is to recognize")
+
+	// 8th Line
+	fmt.Println("that you have a right to an opinion, and you have a right to be heard.")
+
+	// 9th Line
+	fmt.Println("The second step is to start expressing yourself more openly")
+
+	// 10th Line
+	fmt.Println("and practicing speaking up in situations that feel comfortable.")
+
+	// 11th Line
+	fmt.Println("The more you do it, the easier it will become.")
+
+	// 12th Line
+	fmt.Println("Another way to develop your voice is to read, watch, and listen.")
+
+	// 13th Line
+	fmt.Println("Be exposed to different perspectives and ideas,")
+
+	// 14th Line
+	fmt.Println("so that you can form your own opinions based on different sources.")
+
+	// 15th Line
+	fmt.Println("Best of all, be sure to never let anyone silence you.")
+
+	// 16th Line
+	fmt.Println("Confidence is key in finding your voice, and your own opinions and ideas are valuable.")
+
+	// 17th Line
+	fmt.Println("It’s important to be self-assured and believe in yourself.")
+
+	// 18th Line
+	fmt.Println("Finally, don’t be afraid of making mistakes.")
+
+	// 19th Line
+	fmt.Println("Mistakes are part of the process, and you can always learn from them.")
+
+	// 20th Line
+	fmt.Println("Remember that you have something meaningful to say, and don’t be afraid to say it.")
+
+	// 21st Line
+	fmt.Println("It’s important to believe in yourself and find your own voice.")
+
+	// 22nd Line
+	fmt.Println("It may take some practice, but in the end, it will be worth it.")
+
+	// 23rd Line
+	fmt.Println("When you’re confident in your own opinion and voice,")
+
+	// 24th Line
+	fmt.Println("you can have a greater impact on the world around you.")
+
+	// 25th Line
+	fmt.Println("Finding your voice is an important part of finding your identity")
+
+	// 26th Line
+	fmt.Println("and expressing yourself authentically.")
+
+	// 27th Line
+	fmt.Println("It’s important to take the time to practice and find the courage to express yourself.")
+
+	// 28th Line
+	fmt.Println("Have the confidence to make your voice heard and stand up for what you believe in.")
+
+	// 29th Line
+	fmt.Println("Finding your voice is a journey, and there is no one-size-fits-all solution.")
+
+	// 30th Line
+	fmt.Println("Take your time and find the courage to express yourself in your own unique way.")
+
+	// 31st Line
+	fmt.Println("You never know who is listening or what kind of impact you could have.")
+
+	// 32nd Line
+	fmt.Println("Finding your voice can be daunting, but it can also be an incredibly empowering experience.")
+
+	// 33rd Line
+	fmt.Println("Be confident in who you are and what you have to say,")
+
+	// 34th Line
+	fmt.Println("and don’t be afraid to express your true, authentic self.")
+
+	// 35th Line
+	fmt.Println("Have the courage to find your own voice and make it heard.")
+
+	// 36th Line
+	fmt.Println("Believe in yourself, and you will be surprised by how far you can go.")
+
+	// 37th Line
+	fmt.Println("Take the time to practice expressing yourself, and don’t be afraid to make mistakes.")
+
+	// 38th Line
+	fmt.Println("No matter what, have the courage to let your voice be heard.")
+
+	// 39th Line
+	fmt.Println("Take the time to find the courage to express your authentic self,")
+
+	// 40th Line
+	fmt.Println("and you may be surprised by the power of your own voice.")
+
+	// 41st Line
+	fmt.Println("Don’t be afraid to take the plunge and let your voice be heard.")
+
+	// 42nd Line
+	fmt.Println("Finding your voice can be an empowering experience.")
+
+	// 43rd Line
+	fmt.Println("You have the right to an opinion, and you should never feel intimidated to express it.")
+
+	// 44th Line
+	fmt.Println("Be confident in your own opinions and ideas,")
+
+	// 45th Line
+	fmt.Println("and don’t be afraid to stand up for what you believe in.")
+
+	// 46th Line
+	fmt.Println("Take the time to find the courage to express yourself,")
+
+	// 47th Line
+	fmt.Println("and you may be surprised by the power of your own voice.")
+
+	// 48th Line
+	fmt.Println("It takes courage to find your own voice and make it heard.")
+
+	// 49th Line
+	fmt.Println("Trust in yourself and your own opinions, and never let anyone silence you.")
+
+	// 50th Line
+	fmt.Println("Finding your voice can be challenging, but it is essential to finding your identity.")
+
+	// 51st Line
+	fmt.Println("Expose yourself to different opinions and sources,")
+
+	// 52nd Line
+	fmt.Println("so that you can form your own authentic opinions.")
+
+	// 53rd Line
+	fmt.Println("Start by practicing in situations that make you feel comfortable,")
+
+	// 54th Line
+	fmt.Println("and eventually you will find the courage to express yourself openly.")
+
+	// 55th Line
+	fmt.Println("Remember that your opinions are valuable, and you have the right to be heard.")
+
+	// 56th Line
+	fmt.Println("Be self-assured and confident in your voice,")
+
+	// 57th Line
+	fmt.Println("and always remember that you have something meaningful to say.")
+
+	// 58th Line
+	fmt.Println("Don’t be afraid of making mistakes, as they can be valuable learning experiences.")
+
+	// 59th Line
+	fmt.Println("Find your own voice and make it heard.")
+
+	// 60th Line
+	fmt.Println("It’s important to be confident in your opinion,")
+
+	// 61st Line
+	fmt.Println("and to never let anyone silence you.")
+
+	// 62nd Line
+	fmt.Println("Be sure to take the time to practice expressing yourself,")
+
+	// 63rd Line
+	fmt.Println("and you will be surprised by how far you can go.")
+
+	// 64th Line
+	fmt.Println("The journey to finding your voice can be daunting, but it can also be incredibly empowering.")
+
+	// 65th Line
+	fmt.Println("Have the courage to make your voice heard and stand up for what you believe in.")
+
+	// 66th Line
+	fmt.Println("Take the time to find the confidence to express yourself in your own unique way,")
+
+	// 67th Line
+	fmt.Println("and you will be surprised by the power of your own voice.")
+
+	// 68th Line
+	fmt.Println("Finding your voice is essential to finding your identity")
+
+	// 69th Line
+	fmt.Println("and expressing yourself authentically.")
+
+	// 70th Line
+	fmt.Println("It’s important to never let anyone silence you.")
+
+	// 71st Line
+	fmt.Println("Confidence is key in finding your voice, and you have the right to an opinion.")
+
+	// 72nd Line
+	fmt.Println("Start by expressing yourself more openly in comfortable situations")
+
+	// 73rd Line
+	fmt.Println("and be sure to read, watch, and listen, so you can form your own unique opinions.")
+
+	// 74th Line
+	fmt.Println("It’s also important to remember that mistakes are part of the process.")
+
+	// 75th Line
+	fmt.Println("Have the confidence to take the plunge and make your voice heard.")
+
+	// 76th Line
+	fmt.Println("Finding your own voice can be an empowering experience.")
+
+	// 77th Line
+	fmt.Println("Believe in yourself and don’t be afraid of making mistakes.")
+
+	// 78th Line
+	fmt.Println("Trust in yourself and your own opinions, and you will be surprised by how far you can go.")
+
+	// 79th Line
+	fmt.Println("Have the courage to express your authentic self.")
+
+	// 80th Line
+	fmt.Println("No matter what, take the time to find the confidence to make your voice heard.")
+
+	// 81st Line
+	fmt.Println("It’s important to be self-assured and believe in yourself.")
+
+	// 82nd Line
+	fmt.Println("Don’t be intimidated to express your opinion, and practice speaking up.")
+
+	// 83rd Line
+	fmt.Println("The more you do it, the easier it will become.")
+
+	// 84th Line
+	fmt.Println("Be confident in who you are and what you have to say,")
+
+	// 85th Line
+	fmt.Println("and you will be surprised by the power of your own voice.")
+
+	// 86th Line
+	fmt.Println("Finding your voice can be intimidating, but it is essential to finding your identity.")
+
+	// 87th Line
+	fmt.Println("Have the courage to make it heard, and never let anyone silence you.")
+
+	// 88th Line
+	fmt.Println("Start by expressing yourself in comfortable situations,")
+
+	// 89th Line
+	fmt.Println("and take the time to practice and form your own unique opinions.")
+
+	// 90th Line
+	fmt.Println("Remember that you have something valuable to say,")
+
+	// 91st Line
+	fmt.Println("and never let anyone silence you.")
+
+	// 92nd Line
+	fmt.Println("Find the courage to express yourself, and take the time to find your own voice.")
+
+	// 93rd Line
+	fmt.Println("You have the right to an opinion, and it’s important to be confident in your own.")
+
+	// 94th Line
+	fmt.Println("Be sure to read, watch, and listen, so that you can form your own opinions.")
+
+	// 95th Line
+	fmt.Println("It’s also important to remember that mistakes are part of the process.")
+
+	// 96th Line
+	fmt.Println("Take the time to practice expressing yourself,")
+
+	// 97th Line
+	fmt.Println("and you will be surprised by the power of your own voice.")
+
+	// 98th Line
+	fmt.Println("Have the confidence to make it heard,")
+
+	// 99th Line
+	fmt.Println("and stand up for what you believe in.")
+
+	// 100th Line
+	fmt.Println("Finding your voice can be a journey, but it will be worth it in the end.")
+
+	// 101st Line
+	fmt.Println("No matter what, never let anyone silence you.")
+
+	// 102nd Line
+	fmt.Println("Trust in yourself and your own opinions,")
+
+	// 103rd Line
+	fmt.Println("and take the time to find the courage to express yourself authentically.")
+
+	// 104th Line
+	fmt.Println("The journey to finding your voice can be daunting, but it can also be incredibly empowering.")
+
+	// 105th Line
+	fmt.Println("Take the plunge and make your voice heard.")
+
+	// 106th Line
+	fmt.Println("Be sure to expose yourself to different perspectives and sources,")
+
+	// 107th Line
+	fmt.Println("so that you can form your own unique opinions.")
+
+	// 108th Line
+	fmt.Println("It’s important to be self-assured and confident in your voice.")
+
+	// 109th Line
+	fmt.Println("Have the courage to express yourself and never let anyone silence you.")
+
+	// 110th Line
+	fmt.Println("Finding your voice is an essential part of finding your identity,")
+
+	// 111st Line
+	fmt.Println("and it can be an incredibly empowering experience.")
+
+	// 112th Line
+	fmt.Println("Be sure to take the time to practice and find the courage to make it heard.")
+
+	// 113th Line
+	fmt.Println("Remember that you have something meaningful to say,")
+
+	// 114th Line
+	fmt.Println("and don’t be afraid of making mistakes.")
+
+	// 115th Line
+	fmt.Println("It’s important to be confident in your opinion")
+
+	// 116th Line
+	fmt.Println("and to trust in yourself and your own opinions.")
+
+	// 117th Line
+	fmt.Println("Start by expressing yourself more openly in comfortable situations,")
+
+	// 118th Line
+	fmt.Println("and you will be surprised by how far you can go.")
+
+	// 119th Line
+	fmt.Println("Be sure to read, watch, and listen, so that you can form your own authentic opinions.")
+
+	// 120th Line
+	fmt.Println("No matter what, take the time to find the courage to make your voice heard.")
+
+	// 121st Line
+	fmt.Println("Finding your own voice can be an incredibly empowering experience.")
+
+	// 122nd Line
+	fmt.Println("Have the confidence to express yourself and stand up for what you believe in.")
+
+	// 123rd Line
+	fmt.Println("It’s important to never let anyone silence you.")
+
+	// 124th Line
+	fmt.Println("Trust in yourself and your own opinions, and you will be surprised by how far you can go.")
+
+	// 125th Line
+	fmt.Println("Take the plunge and let your voice be heard.")
+
+	// 126th Line
+	fmt.Println("Remember that mistakes are part of the process,")
+
+	// 127th Line
+	fmt.Println("and you can always learn from them.")
